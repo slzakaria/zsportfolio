a345be92f75d6a64605426f74e0c761cee0f3770
zackari sl. <zackaria.celimane@gmail.com>
2024-08-11 21:12:34 +0100

    changed gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,13 +12,12 @@ export default function Document() {
 					rel='stylesheet'
 				/>
 				<Script id='google-tag-manager' strategy='afterInteractive'>
-					{`(function (w, d, s, l, i) {
-						w[l] = w[l] || []; w[l].push({'gtm.start':
-						new Date().getTime(), event: 'gtm.js'}); var f = d.getElementsByTagName(s)[0],
-						j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-						'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-						})(window, document, 'script', 'dataLayer', 'GTM-TX45GD23');
-        `}
+					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-5VN9RWGN');
+					`}
 				</Script>
 				{(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && (
 					// eslint-disable-next-line @next/next/no-sync-scripts
@@ -32,8 +31,8 @@ export default function Document() {
 			<body>
 				<noscript
 					dangerouslySetInnerHTML={{
-						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TX45GD23"
-				height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VN9RWGN"
+						height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
 					}}></noscript>
 				<Main />
 				<NextScript />
