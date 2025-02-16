c3973e38cf3a35ea958746d52dec194306daffbf
zackari sl. <zackaria.celimane@gmail.com>
2025-02-16 16:49:11 +0100

    fixed dockerifle

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
+// next.config.js
+
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+	},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'mdx'],
+	reactStrictMode: true,
+	poweredByHeader: false,
+	output: 'standalone',
+	devIndicators: {
+		buildActivity: true,
+		buildActivityPosition: 'bottom-right',
+	},
+	pageExtensions: ['js', 'jsx', 'mdx'],
 };
 
 module.exports = withMDX(nextConfig);
